7ae1be6bac4d5dd2145289181af21f4efc279abb
Bitajune <beheshtibita@gmail.com>
2021-06-23 10:18:24 -0700

    toggle between players

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // selecting elements by their id or class
+
 const score0El = document.getElementById("score--0")
 const score1El = document.getElementById("score--1")
 const current0El = document.getElementById("current--0")
@@ -10,6 +11,8 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 
 // starting conditions
@@ -17,8 +20,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
-
+let activePlayer = 0;
 
 // dice roll logic
 btnRoll.addEventListener("click", function() {
@@ -30,8 +34,12 @@ diceEl.src = `dice-${dice}.png`
  // 3. check for rolled 1: if true, switch to next player
  if(dice !== 1) {
 currentScore += dice;
-current0El.textContent = currentScore; 
+document.getElementById(`current--${activePlayer}`).textContent = currentScore;
  } else {
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+activePlayer = activePlayer === 0 ? 1 : 0;
+player0El.classList.toggle("player--active");
+player1El.classList.toggle("player--active");
  }
 })
