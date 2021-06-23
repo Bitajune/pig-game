67b6367eec6eec1c26c5075d7b2823b1fae7eb20
Bitajune <beheshtibita@gmail.com>
2021-06-23 11:04:36 -0700

    hold button

--- script.js
+++ script.js
@@ -24,6 +24,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+activePlayer = activePlayer === 0 ? 1 : 0;
+player0El.classList.toggle("player--active");
+player1El.classList.toggle("player--active");
+};
+
 // dice roll logic
 btnRoll.addEventListener("click", function() {
  // 1. generate random dice roll
@@ -35,11 +43,23 @@ diceEl.src = `dice-${dice}.png`
  if(dice !== 1) {
 currentScore += dice;
 document.getElementById(`current--${activePlayer}`).textContent = currentScore;
- } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-activePlayer = activePlayer === 0 ? 1 : 0;
-player0El.classList.toggle("player--active");
-player1El.classList.toggle("player--active");
+ } else { 
+     switchPlayer();
  }
 })
+
+// hold button functionality
+btnHold.addEventListener("click", function() {
+    // 1. add current score to active player
+scores[activePlayer] += currentScore;
+document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    // 2. check if player's score is 100
+if (score[activePlayer] >= 20) {
+    document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+    document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+}
+    // 3. else -> switch player
+    else {
+        switchPlayer();
+    }
+});
